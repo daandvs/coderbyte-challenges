66ba0ff0d936611d40494c2aaaea2ac9edaa9e3f
Daan Devos <daan.devos@vintage.eu>
2014-12-28 16:00:19 +0100

    easy: check sums // first attempt

--- easy/07-simple_symbols.js
+++ easy/07-simple_symbols.js
@@ -1,3 +1,8 @@
+// Take the str parameter being passed and determine if it is an acceptable sequence by either returning
+// the string true or false. The str parameter will be composed of + and = symbols with several letters between them
+// (ie. ++d+===+c++==a) and for the string to be true each letter must be surrounded by a + symbol.
+// So the example to would return false.
+
 function SimpleSymbols(str) {
   str = str.replace(/\+/g, "++");
   str = str.replace(/\+[A-Za-z]\+/g, "");
@@ -6,6 +11,4 @@ function SimpleSymbols(str) {
   return match;
 }
 
-// keep this function call here
-// to see how to enter arguments in JavaScript scroll down
 SimpleSymbols("+d+d+d+d+");
