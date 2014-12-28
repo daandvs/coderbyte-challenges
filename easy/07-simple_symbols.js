c7e66bbdf9e143d32507d500ba706796ccd5c679
Daan Devos <daan.devos@vintage.eu>
2014-12-28 15:39:44 +0100

    easy: simple symbols / second attempt

--- easy/07-simple_symbols.js
+++ easy/07-simple_symbols.js
@@ -2,11 +2,11 @@ function SimpleSymbols(str) {
 
   str = str.replace(/\+[a-zA-z]\+/g, "");
 
-  var match = str.match(/[a-zA-z]/g) ? true : false;
+  var match = str.match(/[a-zA-z]/g) ? false : true;
   return match;
 
 }
 
 // keep this function call here
 // to see how to enter arguments in JavaScript scroll down
-SimpleSymbols("+d+=3=+s+");
+SimpleSymbols("+d===+a+");
