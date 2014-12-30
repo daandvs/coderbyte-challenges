aef2f894b924a6f762238382c28e1b96250350bc
Daan Devos <daan.devos@vintage.eu>
2014-12-30 14:18:48 +0100

    easy: swap case / first attempt

---
+++ easy/24-swap_case.js
@@ -0,0 +1,20 @@
+// take the str parameter and swap the case of each character.
+// letters, numbers and symbols stay the way they are
+
+function SwapCase(str) {
+
+  var newstr = str.replace(/([A-Z])|([a-z])/g, function(match, p1, p2) {
+    if(p1) {
+      return p1.toLowerCase();
+    }
+
+    if(p2) {
+      return p2.toUpperCase();
+    }
+  });
+
+  return newstr;
+
+}
+
+SwapCase("Sup DUDE!!?");
