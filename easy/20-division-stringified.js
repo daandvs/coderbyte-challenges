46f3f339ca7609321530a8e918c7721c5e3fa1c5
Daan Devos <daan.devos@vintage.eu>
2014-12-30 09:12:24 +0100

    easy: division stringified / first attempt

---
+++ easy/20-division-stringified.js
@@ -0,0 +1,24 @@
+function DivisionStringified(num1,num2) {
+
+  var division = Math.round(num1 / num2).toString(),
+      parts = Math.floor(division.length / 3),
+      partsArr = [];
+
+  if(parts !== 0) {
+    for (var i = parts; i >= parts; i--) {
+      partsArr.unshift(division.substr(i-4, 3));
+    }
+  }
+
+
+  var mod = division.length % 3;
+  if(mod > 0) {
+    partsArr.unshift(division.substr(0, division.length % 3));
+  }
+
+  return partsArr.toString();
+}
+
+// keep this function call here
+// to see how to enter arguments in JavaScript scroll down
+DivisionStringified(2, 3);
