0e0a47c41c320153e75a90592a1521180bf1fd8a
Daan Devos <daan.devos@vintage.eu>
2015-01-02 12:02:42 +0100

    easy: power of two / first attempt

---
+++ easy/27-power_two.js
@@ -0,0 +1,9 @@
+function PowersofTwo(num) {
+  var sqrt = Math.floor(Math.sqrt(num));
+
+  return Math.pow(sqrt, 2) === num;
+}
+
+// keep this function call here
+// to see how to enter arguments in JavaScript scroll down
+PowersofTwo(2);
