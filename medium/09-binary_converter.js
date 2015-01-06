59d3a60a1ab55efc9836cfeabfa02662db28bf23
Daan Devos <daan.devos@vintage.eu>
2015-01-06 13:24:57 +0100

    medium: binary converter / better

--- medium/09-binary_converter.js
+++ medium/09-binary_converter.js
@@ -1,14 +1,7 @@
 // return the decimal form of the binary value
 
 function BinaryConverter(str) {
-  var output = 0;
-  str.split("").forEach(function(elem, index, arr) {
-    if(elem === "1") {
-      output += Math.pow(2, (arr.length-1) - index);
-    }
-  });
-
-  return output;
+  return parseInt(str, 2);
 }
 
 BinaryConverter("100101");
