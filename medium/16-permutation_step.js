7677300e45c77967f7c104bba119bd620f18104c
Daan Devos <daan.devos@vintage.eu>
2015-01-21 21:39:43 +0100

    added comments

--- medium/16-permutation_step.js
+++ medium/16-permutation_step.js
@@ -2,45 +2,61 @@
 // num using the same digits
 
 function PermutationStep(num) {
-	var arr = num.toString().split(""),
-		max = 0,
-		loop = true,
-		n = i = arr.length - 1;
 
-	function swap(arr, i1, i2) {
-		var temp = arr[i1];
-		arr[i1] = arr[i2];
-		arr[i2] = temp;
-	}
+  var arr = num.toString().split(""), // first make array of every digit in num
+    loop = true, // set loop variable so we can stop the first while loop
+    n = i = arr.length - 1; // set length and position vars
 
-	while (loop && i > 0) {
-		if (Number(arr[i - 1]) < Number(arr[i])) {
-			max = i;
-			loop = false;
 
-			swap(arr, i - 1, i);
+  // change two values's position in array
+  function swap(arr, i1, i2) {
+    var temp = arr[i1];
+    arr[i1] = arr[i2];
+    arr[i2] = temp;
+  }
 
-			do {
-				while (arr[i] > arr[i + 1]) {
-					for (var j = i; j < n; j++) {
-						if (arr[j] > arr[j + 1]) {
-							swap(arr, j, j + 1);
-						}
-					}
-				}
+  while (loop && i > 0) {
+    // loop array from last index to 0
+    // until we find a digit that is smaller then it's right neighbour
+    if (Number(arr[i - 1]) < Number(arr[i])) {
+      // we have found the index which we can start with
+      // to construct the next number so stop looping
+      loop = false;
 
-				i++;
-			} while (i < n);
-		}
+      // swap positions
+      swap(arr, i - 1, i);
 
-		i--;
-	}
+      // loop from current index to end of array
+      do {
 
-	if (loop) {
-		return -1;
-	}
+        while (arr[i] > arr[i + 1]) {
+          // while digit at current index is greater than digit at next index
+          // loop from current index until end of array
+          for (var j = i; j < n; j++) {
+            // swap digits if digit at current index is greater than digit at next index
+            if (arr[j] > arr[j + 1]) {
+              swap(arr, j, j + 1);
+            }
+          }
+        }
 
-	return Number(arr.join(""));
+        // digit at current index is smaller than digit at next index so
+        // we can move the index to the right and repeat steps until we have
+        // reached the end of the array
+        i++;
+      } while (i < n);
+    }
+
+    i--;
+  }
+
+  // if there's no digit that is smaller than it's right neighboor
+  // loop is still true and we return -1
+  if (loop) {
+    return -1;
+  }
+
+  return Number(arr.join(""));
 }
 
 PermutationStep(12453);
